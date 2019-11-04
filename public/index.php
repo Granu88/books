a264602d8bc36d8073f1395fdaa7a3805209bb9a
David VALLON <davidvallon@hotmail.fr>
2019-11-04 11:34:31 +0100

    Liste+colonne

--- public/index.php
+++ public/index.php
@@ -3,12 +3,16 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <meta name="viewport" content="width=device-width, user-scalable=no">
         <meta charset="utf-8" />
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <link rel="stylesheet" href="/books/views/books.css">
         <title><?php echo $title ?></title>
     </head>
 
     <body>
-        <?php echo $content ?>
+
+      <?php echo $content ?>
+
     </body>
 </html>
--- views/books.php
+++ views/books.php
@@ -1,17 +1,38 @@
 <!-- entre ob_start et ob_get_clean on fait la partie qui change sur la view -->
 <?php $title = 'Liste des livres'; ?>
 <?php ob_start(); ?>
-<h1>Liste des livres</h1>
 
-<ul>
-  <?php
-    foreach ($books as $book) {
-    ?>
-    <li><?php echo $book['title']; ?></li>
-    <?php
-    }
-  ?>
-</ul>
+<h1>Liste des livres</h1>
+  <div class="container">
+    <div class="row">
+      <?php foreach ($books as $book) { ?>
+        <div class="col-md-3 mt-3">
+          <div class="card border-info mb-3 text-center" style="max-width: 18rem;">
+            <div class="card-header bg-dark border-success">
+              <div class="titre">
+                <h5> <?php echo $book ['title'] ?> </h5>
+              </div>
+            </div>
+              <img src=<?php echo $book ['imageLink']; ?> class="card-img-top" alt="photo" style="width:10rem;height: 18rem;">
+              <div class="card-body">
+                <h6> <?php echo $book ['author']  ?></h6>
+              </div>
+              <div class="card-footer bg-transparent border-info">
+                <div class="description">
+                  <button type="button" class="btn btn-info">Description</button>
+                </div>
+                <br>
+                <div class="panier">
+                  <button type="button" class="btn btn-primary">Ajouter au panier</button>
+                </div>
+              </div>
+          </div>
+        </div>
+      <?php } ?>
+    </div>
+
+  </div>
+
 <pre>
 <?php var_dump($books); ?>
 </pre>
