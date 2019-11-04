661c9686311448b6fa1e37d340e16cf94dc51e4d
David VALLON <davidvallon@hotmail.fr>
2019-11-04 12:14:55 +0100

    liste+colonne2

--- views/books.php
+++ views/books.php
@@ -2,30 +2,44 @@
 <?php $title = 'Liste des livres'; ?>
 <?php ob_start(); ?>
 
+<style>
+  .card-header {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100px;
+    overflow: hidden;
+  }
+</style>
+
 <h1>Liste des livres</h1>
   <div class="container">
     <div class="row">
       <?php foreach ($books as $book) { ?>
-        <div class="col-md-3 mt-3">
+        <div class="col-lg-3 col-md-6 mt-3">
           <div class="card border-info mb-3 text-center" style="max-width: 18rem;">
-            <div class="card-header bg-dark border-success">
+            <div class="card-header border-success">
               <div class="titre">
-                <h5> <?php echo $book ['title'] ?> </h5>
+                <h5> <?php echo $book['title']; ?> </h5>
               </div>
             </div>
-              <img src=<?php echo $book ['imageLink']; ?> class="card-img-top" alt="photo" style="width:10rem;height: 18rem;">
-              <div class="card-body">
+              <div class="image">
+                <img src=<?php echo $book ['imageLink']; ?> class="card-img-top" alt="photo" style="width:10rem;height: 18rem;">
+              </div>
+            <div class="card-body">
+              <div class="auteur">
                 <h6> <?php echo $book ['author']  ?></h6>
               </div>
-              <div class="card-footer bg-transparent border-info">
-                <div class="description">
-                  <button type="button" class="btn btn-info">Description</button>
-                </div>
-                <br>
-                <div class="panier">
-                  <button type="button" class="btn btn-primary">Ajouter au panier</button>
-                </div>
+            </div>
+            <div class="card-footer bg-transparent border-info">
+              <div class="description">
+                <button type="button" class="btn btn-info">Description</button>
               </div>
+              <br>
+              <div class="panier">
+                <button type="button" class="btn btn-primary">Ajouter au panier</button>
+              </div>
+            </div>
           </div>
         </div>
       <?php } ?>
